3705c37a1a2aabd0681efbc2f588b1e7c44b5902
Gearnode <friminb@gmail.com>
2017-08-28 12:26:05 +0200

    Fix compiling error with elixir 1.5

--- lib/headers/x_xss_protection.ex
+++ lib/headers/x_xss_protection.ex
@@ -1,67 +1,67 @@
 defmodule SecureHeaders.XXssProtection do
 
-  @moduledoc '''
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, report: "http://google.com/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block", report: "http://google.com/report"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block", report: "/report/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "0"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "0;"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1;"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1;mode=block"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; report=http://google.com/"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; report=/google.com/"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block; report=/google.com/"])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block; report=http://google.com/"])
- 
- 
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, report: "http://google.com/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block", report: "http://google.com/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block", report: "/report/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "allow"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "allow", report: "http://google.com/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "block"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, modes: "block"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "allow"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, report: "google.com"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "allow", report: "http://google.com/"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "block", report: "google.com"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [values: 2]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, reportz: "google.com"]])
- IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [valuez: 2, mode: "block", reportz: "google.com"]])
-  '''
+  @moduledoc """
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, report: "http://google.com/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block", report: "http://google.com/report"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "block", report: "/report/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "0"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "0;"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1;"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1;mode=block"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; report=http://google.com/"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; report=/google.com/"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block; report=/google.com/"])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: "1; mode=block; report=http://google.com/"])
+
+
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, report: "http://google.com/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block", report: "http://google.com/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 0, mode: "block", report: "/report/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "allow"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 1, mode: "allow", report: "http://google.com/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "block"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, modes: "block"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "allow"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, report: "google.com"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "allow", report: "http://google.com/"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, mode: "block", report: "google.com"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [values: 2]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [value: 2, reportz: "google.com"]])
+  IO.inspect( SecureHeaders.XXssProtection.validate [x_xss_protection: [valuez: 2, mode: "block", reportz: "google.com"]])
+  """
   @error_msg "Invalid configuration for x-xss-protection"
   @secure_config [
     value: 0,
     mode: "block",
     report: ""
-  ]    
+  ]
 
   def validate(options) when is_list(options) do
-    case Keyword.has_key?(options, :config) do 
+    case Keyword.has_key?(options, :config) do
       false -> {:ok, options}
-      true  -> 
-      case Keyword.has_key?(options[:config], :x_xss_protection) do
-        # No x-xss-protection configuration found - return config
-        false -> {:ok, options}
-        true  ->
-        case validate_config(options[:config][:x_xss_protection]) do
-          false -> {:error, @error_msg}
-          true  -> {:ok, config_to_string(options, options[:config][:x_xss_protection])}
+      true  ->
+        case Keyword.has_key?(options[:config], :x_xss_protection) do
+          # No x-xss-protection configuration found - return config
+          false -> {:ok, options}
+          true  ->
+            case validate_config(options[:config][:x_xss_protection]) do
+              false -> {:error, @error_msg}
+              true  -> {:ok, config_to_string(options, options[:config][:x_xss_protection])}
+            end
         end
-      end 
     end
-  end  
-  
+  end
+
   def validate(_),  do: {:error, @error_msg}
 
   defp config_to_string(options, xss_config) when xss_config |> is_list do
-    config = Keyword.drop(options[:config], [:x_xss_protection]) 
+    config = Keyword.drop(options[:config], [:x_xss_protection])
     config = config ++ [x_xss_protection: make_string(xss_config)]
     Keyword.drop(options, [:config]) ++ [config: config]
   end
@@ -74,7 +74,7 @@ defmodule SecureHeaders.XXssProtection do
     case validate_keys(xss_config) do
       false -> false
       true  -> validate_config(make_string(xss_config))
-    end  
+    end
   end
 
   defp validate_config(xss_config) when xss_config |> is_bitstring do
@@ -82,7 +82,7 @@ defmodule SecureHeaders.XXssProtection do
     vm  = ~r/^[1](;|; ){1,1}mode=block\z/i
     vr= ~r/^[1](;|; ){1,1}report=.+\z/i
     vmr = ~r/^[1](;|; ){1,1}mode=block(;|; ){1,1}report=.+\z/i
-    Regex.match?(v, xss_config) || Regex.match?(vm, xss_config)|| Regex.match?(vr, xss_config) || Regex.match?(vmr, xss_config)  
+    Regex.match?(v, xss_config) || Regex.match?(vm, xss_config)|| Regex.match?(vr, xss_config) || Regex.match?(vmr, xss_config)
   end
 
   defp validate_config(_), do: {:error, @error_msg}
@@ -90,7 +90,7 @@ defmodule SecureHeaders.XXssProtection do
   defp validate_keys(xss_config) when xss_config |> is_list do
     List.foldl( Keyword.keys(xss_config), true, fn (key,acc) -> List.keymember?(@secure_config,key,0) && acc end)
   end
-  
+
   defp make_string(xss_config) do
     result = ""
     if Keyword.get(xss_config, :value), do: result = result <> Integer.to_string(xss_config[:value])
