31aca30fad1694c000670b6b61fb4d08480dfc92
Gearnode <friminb@gmail.com>
2017-08-28 12:27:15 +0200

    Fix waring compile and indent issue

--- lib/headers/public_key_pins.ex
+++ lib/headers/public_key_pins.ex
@@ -1,6 +1,6 @@
 defmodule SecureHeaders.PublicKeyPins do
 
-  @valid_header  ~r/^((pin-sha256=\S+)\s*){2,}\s*max-age=\d+(;\s*?includeSubdomains)?(;\s*?report-uri=\S+)?/  
+  @valid_header  ~r/^((pin-sha256=\S+)\s*){2,}\s*max-age=\d+(;\s*?includeSubdomains)?(;\s*?report-uri=\S+)?/
   @error_msg "Invalid configuration for public-key-pins"
   @secure_config [
   	pin_sha256: "",
@@ -10,9 +10,9 @@ defmodule SecureHeaders.PublicKeyPins do
   ]
 
   def validate(options) when is_list(options) do
-    case Keyword.has_key?(options, :config) do 
+    case Keyword.has_key?(options, :config) do
       false -> {:ok, options}
-      true  -> 
+      true  ->
     	case Keyword.has_key?(options[:config], :http_public_key_pins) do
       	# No http-public-key-pins configuration found - return config
       	false	-> {:ok, options}
@@ -21,17 +21,17 @@ defmodule SecureHeaders.PublicKeyPins do
         	false -> {:error, @error_msg}
       	  true  -> {:ok, options}
         end
-      end 
+      end
     end
-  end  
-  
+  end
+
   def validate(_),  do: {:error, @error_msg}
 
   defp validate_config(config) when is_bitstring(config) do
   	Regex.match?( @valid_header, config)
   end
 
-  defp validate_config(config) do
+  defp validate_config(_config) do
   # TODO implement keyword list to string function
   	false
   end
--- lib/secure_headers.ex
+++ lib/secure_headers.ex
@@ -1,5 +1,5 @@
 defmodule SecureHeaders do
-  
+
   use Pipe
 
   import Plug.Conn#, only: [halt: 1, delete_resp_header: 2, update_resp_header: 4, assign: 3]
@@ -7,9 +7,9 @@ defmodule SecureHeaders do
   @doc "Callback implementation for Plug.init/1"
   def init(options) do
     options
-      |> merge?
-      |> merge!(options)
-      |> validate
+    |> merge?
+    |> merge!(options)
+    |> validate
   end
 
   @doc "Callback implementation for Plug.call/2"
@@ -19,7 +19,7 @@ defmodule SecureHeaders do
       _   -> set_headers(conn, options)
     end
   end
-  
+
   defp validate(options) do
     pipe_matching x, {:ok, x},
       options
@@ -34,94 +34,75 @@ defmodule SecureHeaders do
       |> SecureHeaders.XXssProtection.validate
       |> SecureHeaders.ClearSiteData.validate
   end
-  
-  defp set_headers(conn, options) when options |> is_list do
+
+  defp set_headers(conn, options) when is_list(options) do
     headers = get_config(options)
-    conn 
-        |> delete(headers)
-        |> set(headers)
-  end
-
-  defp set_headers(conn, _) do
     conn
+    |> delete(headers)
+    |> set(headers)
   end
+  defp set_headers(conn, _), do: conn
 
-    
+
   defp set(conn, nil), do: conn
-  
+
   defp set(conn, list) when list |> is_list do
     List.foldl(list, conn, fn({key, value}, conn) -> set(conn, dasherize(key), value) end)
   end
-  
+
   defp set(conn, _key, nil), do: conn
-  
+
   defp set(conn, key, value) when value |> is_bitstring do
     conn = conn |> assign(make_atom(key), value)
     update_resp_header(conn, key, value, fn(_) -> value end)
   end
-  
+
   defp delete(conn, nil), do: conn
-  
   defp delete(conn, []), do: conn
-  
-  defp delete(conn, [h|t]) when h |> is_bitstring do
-    conn
-    |> delete_resp_header(h)
-    |> delete(t)
-  end
-    
-  defp delete(conn, list) when list |> is_list do
+  defp delete(conn, key) when key |> is_bitstring, do: delete(conn, [key])
+  defp delete(conn, [h|t]) when is_bitstring(h), do: conn |> delete_resp_header(h) |> delete(t)
+  defp delete(conn, list) when is_list(list) do
     List.foldl(list, conn, fn({key, _}, conn) -> delete(conn, dasherize(key)) end)
   end
 
-  defp delete(conn, key) when key |> is_bitstring, do: delete(conn, [key])
-  
+
   defp dasherize(data) when is_atom(data), do: dasherize(Atom.to_string(data))
-  
   defp dasherize(data), do: String.replace(data, "_", "-")
 
   defp make_atom(string), do: String.to_atom(String.replace(string, "-", "_"))
 
   defp get_config(options), do: get_config(options, [])
-  
   defp get_config(options, default_value) do
     case get_in(options, [:secure_headers, :config]) do
       nil -> default_value
       _   -> get_in(options, [:secure_headers, :config])
     end
   end
-  
+
   defp set_config(options, config) when options |> is_list do
     if Keyword.has_key?(options, :secure_headers) do
     	options = Keyword.delete(options[:secure_headers], :config)  ++ [config: config]
     end
     [secure_headers: options]
-  end    
-    
+  end
+
   defp merge?(options), do: get_in(options, [:secure_headers, :merge])
-    
+
   defp merge_options(options) do
-    env_options = Application.get_env(:secure_headers, SecureHeaders, [])    
+    env_options = Application.get_env(:secure_headers, SecureHeaders, [])
     merged_options = Keyword.merge(env_options[:secure_headers], options[:secure_headers])
     [secure_headers: merged_options]
   end
-    
+
   defp merge_config(options) do
-    env_options = Application.get_env(:secure_headers, SecureHeaders, [])    
+    env_options = Application.get_env(:secure_headers, SecureHeaders, [])
     env_config = get_config(env_options)
-    config = get_config(options)    
+    config = get_config(options)
    	merged_config = Keyword.merge(env_config, config)
   	set_config(options, merged_config)
   end
-    
-  defp merge!(boolean, options) do
-	if(boolean) do
-    options =
-	  options 
-        |> merge_config
-        |> merge_options
-    end
-    options
-  end
-end  
+
+  defp merge!(true, options), do: options |> merge_config |> merge_options
+  defp merge!(_bool, options), do: options
+end
 
